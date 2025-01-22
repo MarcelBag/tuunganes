5df885b1dcdde8bfeeb0f6dd8d9510d021eae952
Marcel bag <marcelshabani@gmail.com>
2025-01-22 09:01:39 +0200

    restructuring the project

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder='backend/templates', #ttemplates path
+    static_folder='static'  # static files
+)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
